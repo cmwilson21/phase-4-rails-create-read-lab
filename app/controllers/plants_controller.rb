e0878b8dfb1f95d1d1af8fc6cf70e7e7d3ac15ce
Courtney Wilson <cmwilson132@gmail.com>
2021-09-08 21:06:47 -0500

    completed

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,29 @@
+class PlantsController < ApplicationController
+
+  def index
+    plants = Plant.all
+    render json: plants
+  end
+
+  def show
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      render json: plant
+    else
+      render json: {error: "Plant not found"}
+    end
+  end
+
+  def create
+    plant = Plant.create(plant_params)
+    render json: plant
+  end
+
+
+  private
+
+  def plant_params
+    params.permit(:name, :image, :price)
+  end
+
+end
